c911e17a4c82cd2cfb08054315281f66e4b3ebdf
Samuel James <contactchenke@gmail.com>
2012-05-26 14:43:39 +0800

    new

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#initialization system
+mkdir ~/git ~/downloads ~/pictures
+
+#configure system
+cp ./conf/init/* ~/
+cp ./conf/openbox/* ~/.config/openbox/
+cp ./img/pictures/ -r ~/
+cp ./conf/vim/* ~/
+cp ./conf/aliedit/* ~/.mozilla/plugins/
+
+#install applications
+
+pacman -Syu xorg xorg-xinit xf86-video-nouveau alsa-utils firefox \
+firefox-i18n-zh-cn openbox ibus-pinyin openssh xcompmgr rxvt-unicode \
+flashpluign wqy-zenhei ttf-dejavu ttf-arphic-uming vim feh git sudo \
+scrot gimp
+
