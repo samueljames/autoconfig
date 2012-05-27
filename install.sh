cd473f3c837f1fd9dc5b3d29c28af5ad550fea32
Samuel James <contactchenke@gmail.com>
2012-05-27 12:38:29 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -17,8 +17,6 @@ firefox-i18n-zh-cn openbox ibus-pinyin openssh xcompmgr rxvt-unicode \
 flashplugin wqy-zenhei ttf-dejavu ttf-arphic-uming vim feh git sudo \
 scrot gimp
 
-else
-
 #alidit libpng debug
 ln -sf /usr/lib/libpng /usr/lib/libpng12.so.0
 
@@ -29,10 +27,11 @@ if [ "$select" = "yes" ]; then
 
 read -p "do you have a user? (yes or no): " haveuser
 echo -e "\n"
-else
+else if [ "$install" = "no" ]; then
 	echo "may be you just need install base applications"
 	exit 1
-fi  
+fi
+fi
 
 if [ "$haveuser" = "yes" ]; then
 	read -p "user name is? [default:samuel]: " username
@@ -40,6 +39,7 @@ if [ "$haveuser" = "yes" ]; then
 else
 	echo "you are using root,maybe you need run startx"
 	username="root"
+fi
 
 if [ "$username" = "" ]; then
 	username="samuel"
@@ -48,6 +48,7 @@ if [ "$username" = "" ]; then
 	mkdir /home/$username/git 
 	mkdir /home/$username/downloads 
 	mkdir -p /home/$username/pictures/.background
+	mkdir -p /home/$username/.config/openbox
 
 	#configure system
 	cp ./conf/init/.xinitrc /home/$username/.xinitrc
@@ -62,7 +63,16 @@ if [ "$username" = "" ]; then
 	cp ./conf/system/mkinitcpio.conf /etc/mkinitcpio.conf
 	cp ./conf/system/rc.conf /etc/rc.conf
 	
-	chown -R $username:$usernmae /home/$username/*
+	chown -R $username:$usernmae /home/$username/.mozilla/plugins/libaliedit64.so
+	chown -R $username:$usernmae /home/$username/.xinitrc
+	chown -R $username:$usernmae /home/$username/.Xresources
+	chown -R $username:$usernmae /home/$username/.config/openbox/autostart
+	chown -R $username:$usernmae /home/$username/.config/openbox/environment
+	chown -R $username:$usernmae /home/$username/.config/openbox/menu.xml
+	chown -R $username:$usernmae /home/$username/.config/openbox/rc.xml
+	chown -R $username:$usernmae /home/$username/.vimrc
+	chown -R $username:$usernmae /home/$username/pictures/.background/bg.jpg
+
 	clear
 	echo -e "\n"
 	echo "your system is set 
