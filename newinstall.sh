8d01350779894051c22e32440a8b2994548ce019
Samuel James <contactchenke@gmail.com>
2012-06-04 20:54:11 +0800

    new install.sh

--- newinstall.sh
+++ newinstall.sh
@@ -7,24 +7,17 @@ if [ $(id -u) != "0" ]; then
  	exit 1
 else
 
-select var in "no run it,debug" "install base applications" "just fix bug" "create a user" "auto configure system" "clear and exit install"; do
+select selectd in "install-base-applications" "just-fix-bug" "create-a-user" "auto-configure-system" "clear-and-exit-install"; do
 
-	while [ "$return" ]; do
-	if [ "$return" = "0" ]; then
+	case $selectd in
 
-	break
-fi
-done
-done
+		install-base-applications)
 
 #install application 
-if [ $var = "install base applications" ]; then
 pacman -Syu xorg xorg-xinit xf86-video-nouveau alsa-utils firefox \
 firefox-i18n-zh-cn openbox ibus-pinyin openssh xcompmgr rxvt-unicode \
 flashplugin wqy-zenhei ttf-dejavu ttf-arphic-uming vim feh git sudo \
 scrot gimp openntpd slim slim-themes conky sysstat unrar unzip
-
-fi
 
 #check install
 if [ -f /var/cache/pacman/pkg/openbox*.tar.xz ]; then
@@ -33,33 +26,33 @@ else
 	echo "you need install base applications"
 	exit 1
 fi
+;;
 
+	just-fix-bug)
 #fix bug
 #alidit libpng fixbug
-if [ "$var" = "just fix bug" ]; then
 ln -sf /usr/lib/libpng /usr/lib/libpng12.so.0
 
 #touch xorg configure file
 Xorg -configure
 mv /root/xorg.conf.new /etc/X11/xorg.conf.d/xorg.conf
 
-fi
+;;
 
+	have-a-user)
 #check have a user
-if [ "$var" = "create a user" ]; then
-
 	read -p "user name is? [default is samuel]: " username
-	echo "create user complete"
 
 if [ "$username" = "" ]; then
 	username="samuel"
 	useradd -m -s /bin/bash $username
+	echo "create user complete"
 
 fi
-fi
+;;
 
+	auto-configure-system)
 #auto configure system
-if [ "$var" = "auto configure system" ]; then
 
 	#initialization user space
 	mkdir /home/$username/git 
@@ -125,13 +118,15 @@ if [ "$var" = "auto configure system" ]; then
 	chown -R $username:$usernmae /home/$username/pictures/.background/bg.jpg
 
 	chown -R $username:$usernmae /home/$username/.bashrc
-fi
+;;
 
+	clear-and-exit-install)
 	#clear and exit install
-	if [ "$var" = "clear and exit install" ]; then
 	clear
 	echo "your system is configure
 maybe you need change to $username and run startx"
-	return="0"
-fi
-fi
+
+;;
+
+*) echo "error";exit 1;;
+esac
